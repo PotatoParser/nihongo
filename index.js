6fcaac2fdf923bff2fd564dd58a8eba4a6b1eefb
PotatoParser <wilsonqnguyen@gmail.com>
2018-12-20 23:19:43 -0800

    0.6.5.1
    + Fixed for external use on NodeJS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var cors = require('cors');
 const _parser = require('body-parser');
 app.use(cors());
-app.use(express.static('C:/Users/MidSh/Desktop/nihongo'));
+app.use(express.static(__dirname));
 app.get("/", (req, res)=>res.sendFile(__dirname + "/test.html"));
 app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'));
 app.use(_parser.json({limit: "50mb"}));
